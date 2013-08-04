62655f166c2f46117fe27bc730ef1c737251463f
zhouhuan <zhouhuan@redflag-linux.com>
2013-08-04 11:47:44 +0800

    rails new softcenter and bundler install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Softcenter::Application.config.secret_key_base = '935f7608dd3c7821759f1b4f342151b637bde71418b3b7957fc868ff28918e1982df77d1b2144155c04a6de65e948691f669d7903ee75098f14dc1f9017f3ca8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Softcenter::Application.config.session_store :cookie_store, key: '_softcenter_session'
